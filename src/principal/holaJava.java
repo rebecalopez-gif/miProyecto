47c36a9810297cc9c3213e25682fa7f60b325b82
rebecalopez-gif <1dami@TartangaLH.eus>
2025-11-03 13:41:07 +0100

    nuevoSyso

--- src/principal/holaJava.java
+++ src/principal/holaJava.java
@@ -1,11 +1,17 @@
 package principal;
 
+import java.util.Scanner;
+
 public class holaJava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		System.out.println("Hola Java");
+		Scanner sc= new Scanner(System.in);
+		sc.nextLine();
+		sc.close();
+		System.out.println("nuevoSyso");
 	}
 
 }
