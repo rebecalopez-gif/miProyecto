3363adb4d7302c70db7363e26feb7fc84c6f0101
rebecalopez-gif <1dami@TartangaLH.eus>
2025-11-03 13:54:01 +0100

    nueva rama

---
+++ src/principal/nota.java
@@ -0,0 +1,17 @@
+package principal;
+
+import java.util.Scanner;
+
+public class nota {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Nueva rama");
+		Scanner sc= new Scanner(System.in);
+		sc.nextLine();
+		sc.close();
+		
+	}
+
+}
